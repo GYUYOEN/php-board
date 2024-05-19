a29add23f0da238755d373bbadcc9065e915ecc2
gyu yoen <rbdus9696@naver.com>
2024-05-20 08:37:29 +0900

    database.php 변경

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,8 +27,8 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => 'ghfaos65',
+        'username'     => '',
+        'password'     => '',
         'database'     => 'php_board',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
